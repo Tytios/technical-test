d64fd58457036a72b073861bd48e3cb926b3d7b5
Françoise <christopher.francoise@fortil.fr>
2022-06-09 23:00:53 +0200

    Initial layers with PrimeNg and PrimeFlex

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//PrimeNg
+import {ButtonModule} from 'primeng/button';
+import {CardModule} from 'primeng/card';
+
+import { MainContainerComponent } from './core/layouts/main-container/main-container.component';
+import { HeaderComponent } from './core/layouts/header/header.component';
+import { HomeComponent } from './core/components/home/home.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainContainerComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    //PrimeNg Module
+    ButtonModule,
+    CardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
